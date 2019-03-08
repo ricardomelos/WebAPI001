47559818a2c700e06f3aab55b5f5cfcce7868f37
ricardomelos <eng.ricardomelos@gmail.com>
2019-03-08 14:26:36 -0300

    criação de webapi de teste

---
+++ Hello2019/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Hello2019.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello World";
+        }
+        public string Get(int id)
+        {
+            var list = new List<string>
+            {
+                "Olá Ricardo",
+                "Hello RIchard",
+                "Salut Ricardo",
+                "Ola que tal Ricardo"
+            };
+
+            return id > list.Count - 1 ? "Índice fora do range" : list[id];
+        }
+    }
+}
---
+++ Hello2019/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hello2019.WebApiApplication" Language="C#" %>
